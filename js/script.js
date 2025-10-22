b208b94efeb24f79568584cabada08f69c011ec5
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-10-22 19:40:33 -0300

    closes #5 - pesquisa de um CEP

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 $('#cepInput').mask('00000-000');
+
+
+// CEP JSON 
+
+
+$("#cepInput").on('blur', function (){
+   
+    var CEP = $("#cepInput").val().replace(/\D/g, '');
+
+    var CEP_API = `https://viacep.com.br/ws/${CEP}/json/`;
+
+    
+    $.getJSON(CEP_API, (cep) => {
+
+        $("#adressInput").val(cep.logradouro);
+        $("#neighbourInput").val(cep.bairro); 
+        $("#cityInput").val(cep.localidade); 
+        $("#stateInput").val(cep.estado);
+        
+    })
+})
+   
