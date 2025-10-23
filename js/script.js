f426422176fa691149398d08c6e3b1503127e4b7
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-10-22 21:23:55 -0300

    realizando parcialmente a tarefa #7

--- js/script.js
+++ js/script.js
@@ -4,21 +4,42 @@ $('#cepInput').mask('00000-000');
 // CEP JSON 
 
 
-$("#cepInput").on('blur', function (){
-   
+$("#cepInput").on('blur', function () {
+
     var CEP = $("#cepInput").val().replace(/\D/g, '');
 
     var CEP_API = `https://viacep.com.br/ws/${CEP}/json/`;
 
-    
     $.getJSON(CEP_API, (cep) => {
 
-        $("#adressInput").val(cep.logradouro);
-        $("#neighbourInput").val(cep.bairro); 
-        $("#cityInput").val(cep.localidade); 
-        $("#stateInput").val(cep.estado);
-        
-        $("#numberInput").prop("disabled", false);
-    })
-})
-   
+        if ("erro" in cep)
+            $("#erro").html("CEP não encontrado!");
+        else {
+            LimpaErro();
+            IncluiValorEndereco(cep);
+        }
+
+    }).fail((jqXHR) => {
+        console.log('Status:', jqXHR.status);
+        LimpaErro();
+
+        if (jqXHR.status === 0)
+            $("#erro").html("CEP inválido!");
+    });
+
+});
+
+function IncluiValorEndereco(cep) {
+
+    $("#adressInput").val(cep.logradouro);
+    $("#neighbourInput").val(cep.bairro);
+    $("#cityInput").val(cep.localidade);
+    $("#stateInput").val(cep.estado);
+
+    // Habilita o input Numero
+    $("#numberInput").prop("disabled", false);
+}
+
+function LimpaErro() {
+    $("#erro").html("");
+}
